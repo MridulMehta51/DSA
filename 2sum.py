c14b89c7f725595918401dee4f1196a3e25ecd6d
MridulMehta51 <133112783+MridulMehta51@users.noreply.github.com>
2024-07-25 12:42:36 -0400

    Update 2sum.py

--- 2sum.py
+++ 2sum.py
@@ -1,13 +1,11 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         num_to_index = {} 
-        
         for i in range(len(numbers)):
             num = numbers[i]
             com = target - num 
-            
             if com in num_to_index:
                 return [num_to_index[com] + 1, i + 1]
-            
             num_to_index[num] = i 
-    
+        return []
+
