464e8c4cafc93faad2e9bf9b4130a3868d860002
MridulMehta51 <133112783+MridulMehta51@users.noreply.github.com>
2024-07-26 14:48:27 -0400

    Create Minimum Size Subarray Sum.py

---
+++ Minimum Size Subarray Sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        a= nums.sort()
+        i=len(nums)-1
+        count=0
+        res=0
+        for i in range(len(nums)):
+            if nums[i]== target:
+                count+=1
+                break
+            elif res > target:
+                res=0
+                i+=1
+            else:
+                res+=nums[i]
+        return count
+                
+        
+        
