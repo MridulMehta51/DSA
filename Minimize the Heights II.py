d6e638c5145db8c7c66253566e1e5ac72822da81
MridulMehta51 <mridulmehta51@gmail.com>
2024-11-19 21:28:00 -0500

    Update Minimize the Heights II.py

--- Minimize the Heights II.py
+++ Minimize the Heights II.py
@@ -3,6 +3,7 @@
 
 # For each tower, you must perform exactly one of the following operations exactly once.
 
+
 # Increase the height of the tower by K
 # Decrease the height of the tower by K
 # Find out the minimum possible difference between the height of the shortest and tallest towers after you have modified each tower.
