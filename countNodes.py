1837a4d35cb3f0fe6dc439afba04d8123e79028e
MridulMehta51 <mridulmehta51@gmail.com>
2024-12-19 15:52:22 -0500

    Create countNodes.py

---
+++ countNodes.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        arr=[]
+        def inorder(node):
+            if node:  
+                inorder(node.left)
+                arr.append(node.val)
+                inorder(node.right)
+        inorder(root)
+        return len(arr)
+        # Second solution
+        # return self.countNodes(root.left)+1+ self.countNodes(root.right)
+
+
+
+        
