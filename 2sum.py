98af209d5a09833c4d0141481e26858fa24118b1
MridulMehta51 <mridulmehta51@gmail.com>
2025-04-21 12:15:57 -0400

    Update 2sum.py

--- 2sum.py
+++ 2sum.py
@@ -10,3 +10,26 @@ class Solution:
             num_to_index[num] = i 
         return []
 
+# Approach 2 
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers)):
+            for j in range(i+1,len(numbers)):
+                if numbers[i]+numbers[j] == target:
+                    return [i+1,j+1]
+        return -1
+
+        
+# Approach 3 
+        left,right=0,len(numbers)-1
+
+        while left<right:
+            current=numbers[left]+numbers[right]
+
+            if current==target:
+                return[left+1,right+1]
+            elif current > target:
+                right-=1
+            else:
+                left+=1
+        return -1
