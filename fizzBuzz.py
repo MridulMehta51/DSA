c416d9ec3210da867cb72fe7b55fb84eaaa7dbce
MridulMehta51 <mridulmehta51@gmail.com>
2025-05-04 23:44:00 -0400

    Update fizzBuzz.py

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -13,6 +13,25 @@ class Solution:
                 res.append(str(i))
         return res
 
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        res=[]
+        for i in range(1,n+1):
+            entry=""
+            if i%3==0:
+                entry+="Fizz"
+            if i%5==0:
+                entry+="Buzz"
+            if not entry:
+                entry+=str(i)
+            res.append(entry)
+        return res
+
+
+
+
+
+        
 
 
 
