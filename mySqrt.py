dffbadbeaa5b1d5e175548b1f2f4b2d5869b4178
MridulMehta51 <mridulmehta51@gmail.com>
2025-06-10 14:54:57 -0400

    Create mySqrt.py

---
+++ mySqrt.py
@@ -0,0 +1,24 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        
+        val=0
+        left,right=1,x
+        
+        
+        while left<=right:
+            m=(left+right)//2
+            
+            square=m*m
+            if x==square:
+                return m
+            elif square<x:
+                
+                left=m+1
+            else:
+                
+                right=m-1
+                
+        return right
+                
+        
+        
